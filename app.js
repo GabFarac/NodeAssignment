67c655245188f1b3c1e3fb17a881ff7101de1ddd
Gabriel <gabriel.farac@outlook.com>
2023-11-08 10:19:18 -0800

    added the app.post route for new books

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 import express from 'express';
+import data from './books.json' assert {type:'json'}
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const data = [
-    {
-      id: 1,
-      title: "Example Book 1",
-      author: "Author 1",
-      year: 2020
-    },
-    {
-      id: 2,
-      title: "Example Book 2",
-      author: "Author 2",
-      year: 2018
-    },
-    {
-      id: 3,
-      title: "Example Book 3",
-      author: "Author 3",
-      year: 2022
-    }
-  ]
+app.use(express.json());
 
 app.get('/books', (req, res) => {
     const titles = data.map(book => book.title)
@@ -33,5 +15,11 @@ app.get('/books/:id', (req, res) => {
     const book = data.filter(book => book.id === Number(id))
     res.status(200).send(book[0])
 });
+
+app.post('/books', (req, res) => {
+  const object = req.body;
+  data.push(object);
+  res.sendStatus(200);
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
