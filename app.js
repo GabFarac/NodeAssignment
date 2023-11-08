3873506a4f02caba1569edc02042a539f1b69733
Gabriel <gabriel.farac@outlook.com>
2023-11-07 18:46:42 -0800

    updated package.json and books titles route added to app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const data = [
+    {
+      id: 1,
+      title: "Example Book 1",
+      author: "Author 1",
+      year: 2020
+    },
+    {
+      id: 2,
+      title: "Example Book 2",
+      author: "Author 2",
+      year: 2018
+    },
+    {
+      id: 3,
+      title: "Example Book 3",
+      author: "Author 3",
+      year: 2022
+    }
+  ]
+
+app.get('/books', (req, res) => {
+    const titles = data.map(book => book.title)
+    res.status(200).send(titles)
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
