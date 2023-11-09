73be59f27008ecc3101d82349b12fa6a9dd95a1e
Gabriel <gabriel.farac@outlook.com>
2023-11-08 19:37:34 -0800

    FileSystem integration added to app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import data from './books.json' assert {type:'json'}
+import  fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const data = JSON.parse(fs.readFileSync('books.json').toString());
 app.use(express.json());
 
 app.get('/books', (req, res) => {
@@ -18,7 +19,29 @@ app.get('/books/:id', (req, res) => {
 
 app.post('/books', (req, res) => {
   const object = req.body;
-  data.push(object);
+  if(!object.title || !object.author || !object.year){
+    return res.sendStatus(422)
+  }
+  let id = data.length+1
+  let flag = 0
+  while(flag === 0){
+    flag = 1
+    for(let i = 0; i < data.length; i++){
+      if(data[i] === data.length){
+        id++
+        flag = 0
+        break
+      }
+    }
+  }
+  const newBook = {
+    id,
+    title: object.title,
+    author: object.author,
+    year: object.year,
+  }
+  data.push(newBook)
+  fs.writeFileSync('books.json', JSON.stringify(data));
   res.sendStatus(200);
 })
 
@@ -33,9 +56,10 @@ app.put('/books/:id', (req, res) => {
         author: object.author || data[i].author,
         year: object.year || data[i].year,
       }
+      break
     }
-    break
   }
+  fs.writeFileSync('books.json', JSON.stringify(data))
   res.sendStatus(200);
 })
 
@@ -44,9 +68,10 @@ app.delete('/books/:id', (req, res) => {
   for(let i = 0; i < data.length; i++){
     if(data[i].id === Number(id)){
       data.splice(i, 1)
+      break
     }
-    break
   }
+  fs.writeFileSync('books.json', JSON.stringify(data))
   res.sendStatus(200);
 })
 
